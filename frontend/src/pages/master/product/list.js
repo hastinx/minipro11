9a6fc4de597ba03986508effaceafd6885e4afb4
MuhammadHastin <hastinre@gmail.com>
2023-04-18 00:39:54 +0700

    Frontend - Product - add Filter init

--- frontend/src/pages/master/product/list.js
+++ frontend/src/pages/master/product/list.js
@@ -9,12 +9,51 @@ const List = () => {
   const [paging, setPaging] = useState([]);
   const [pageNumber, setPN] = useState(1);
   const [number, setNumber] = useState(0);
+  const [sort, setSort] = useState(0);
+  const [category, setCategory] = useState(0);
+  const [product_name, setProductName] = useState('');
 
   const getProducts = async () => {
+    console.log(parseInt(sort));
+    let uri;
+    switch (parseInt(sort)) {
+      case 1:
+        uri =
+          'http://localhost:1234/api/product?page=' +
+          pageNumber +
+          '&id=1&order=nama&sort=DESC';
+        break;
+      case 2:
+        uri =
+          'http://localhost:1234/api/product?page=' +
+          pageNumber +
+          '&id=1&order=nama';
+        break;
+      case 3:
+        uri =
+          'http://localhost:1234/api/product?page=' +
+          pageNumber +
+          '&id=1&order=price&sort=DESC';
+        break;
+      case 4:
+        uri =
+          'http://localhost:1234/api/product?page=' +
+          pageNumber +
+          '&id=1&order=price';
+        break;
+      default:
+        uri =
+          'http://localhost:1234/api/product?page=' +
+          pageNumber +
+          '&id=1&category=' +
+          parseInt(category) +
+          '&product_name=' +
+          product_name;
+    }
+
+    console.log(uri);
     try {
-      const data = await axios.get(
-        'http://localhost:1234/api/product?page=' + pageNumber + '&id=1'
-      );
+      const data = await axios.get(uri);
       console.log(data.data.values);
       setProducts(data.data.values.result);
 
@@ -28,18 +67,68 @@ const List = () => {
       }
       console.log(paginate);
       setPaging(paginate);
-      setRecords(data.data.values.count_data)
+      setRecords(data.data.values.count_data);
       setPN(parseInt(data.data.values.page));
-    } catch (error) { }
+    } catch (error) {}
   };
 
   useEffect(() => {
     getProducts();
-    setNumber(pageNumber - 1)
-  }, [pageNumber]);
+    setNumber(pageNumber - 1);
+  }, [pageNumber, sort, category, product_name]);
   return (
     <Navbar title="Product List">
-      Page {pageNumber} of {records} data
+      <div className="d-flex justify-content-between">
+        <div className="d-flex gap-3">
+          <div className="row">
+            <div className="col-md-12">
+              <select
+                class="form-select"
+                aria-label="Default select example"
+                onChange={(e) => setSort(e.target.value)}
+              >
+                <option value="0" selected>
+                  Sort
+                </option>
+                <option value="1">Sort by name a-z</option>
+                <option value="2">Sort by name z-a</option>
+                <option value="3">Sort by price high-low</option>
+                <option value="4">Sort by price low-high</option>
+              </select>{' '}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <select
+                class="form-select"
+                aria-label="Default select example"
+                onChange={(e) => setCategory(e.target.value)}
+              >
+                <option value="0" selected>
+                  Category
+                </option>
+                <option value="1">Category 1</option>
+                <option value="2">Category 2</option>
+              </select>{' '}
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-12">
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Product Name"
+                onChange={(e) => setProductName(e.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+
+        <span className="fw-bold">
+          Page {pageNumber} of {records} data
+        </span>
+      </div>
+      <hr />
       <table class="table">
         <thead>
           <tr>
@@ -54,12 +143,11 @@ const List = () => {
         </thead>
         <tbody>
           {products.map((i, idx) => (
-
             <tr key={i.id}>
-              <th scope="row">{((pageNumber - 1) * 9) + (idx + 1)}</th>
+              <th scope="row">{(pageNumber - 1) * 9 + (idx + 1)}</th>
               <td>{i.nama}</td>
               <td>{i.deskripsi}</td>
-              <td>{i.category_id}</td>
+              <td>{i.category_name}</td>
               <td>{i.qty}</td>
               <td>{i.price}</td>
               <td>
@@ -77,7 +165,9 @@ const List = () => {
       </table>
       <div id="paginate">
         {pageNumber === 1 ? (
-          <button className="btn btn-outline-primary"><i className="bi bi-chevron-left"></i></button>
+          <button className="btn btn-outline-primary">
+            <i className="bi bi-chevron-left"></i>
+          </button>
         ) : (
           <button
             className="btn btn-outline-primary"
